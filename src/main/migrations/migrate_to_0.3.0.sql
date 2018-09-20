070ecdbb4553605cc584a5e8fbce19cbc7642bb1
vsevolod <seravkin314@gmail.com>
2018-09-21 00:20:43 +0300

    - поправил патч

--- src/main/migrations/migrate_to_0.3.0.sql
+++ src/main/migrations/migrate_to_0.3.0.sql
@@ -1,6 +1,6 @@
 alter table notifications alter column dt_to_notificate set not null;
 
-alter table notifications add column if not exists kind varchar(50);
+alter table notifications add column kind varchar(50);
 
 update notifications
 set kind = 'OneDate'
@@ -8,13 +8,13 @@ where kind is null;
 
 alter table notifications alter column kind set not null;
 
-alter table notifications add column if not exists period bigint;
-alter table notifications add column if not exists hour int;
-alter table notifications add column if not exists minute int;
-alter table notifications add column if not exists start timestamp;
-alter table notifications add column if not exists finish timestamp;
+alter table notifications add column period bigint;
+alter table notifications add column hour int;
+alter table notifications add column minute int;
+alter table notifications add column start timestamp;
+alter table notifications add column finish timestamp;
 -- Normalization needed, but I'm short on time
-alter table notifications add column if not exists days varchar(20);
+alter table notifications add column days varchar(20);
 
 alter table notifications add constraint notifications_kind_correct
 check (notifications.kind in ('OneDate', 'Confirmation', 'Periodic'));
